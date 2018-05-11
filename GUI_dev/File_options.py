7c7de79e1c461b701948191778b4ff366d58b357
Robert Castilla <robert.castilla@upc.edu>
2018-05-11 16:28:43 +0200

    Corrected for compatibility with python 3

--- GUI_dev/File_options.py
+++ GUI_dev/File_options.py
@@ -7,18 +7,18 @@ from PyQt5.QtWidgets import QMessageBox
 from PyFoam.Applications.CloneCase import CloneCase
 
 def NewCase (name):
-	os.system("pyFoamCloneCase.py $FOAM_TUTORIALS/incompressible/simpleFoam/motorBike $FOAM_RUN/" + name)
+    os.system("pyFoamCloneCase.py $FOAM_TUTORIALS/incompressible/simpleFoam/motorBike $FOAM_RUN/" + name)
 
 def ShowDialog ():
-	msg = QMessageBox()
-   	msg.setIcon(QMessageBox.Information)
-	msg.setText("File structure for the new case has been created!")
-   	#msg.setInformativeText("This is additional information")
-   	msg.setWindowTitle("New Case")
-   	msg.setDetailedText("Directory created in OpenFOAM home directory: $FOAM_RUN")
-   	msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-	retval = msg.exec_()
-   	print "value of pressed message box button:", retval
-	
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Information)
+    msg.setText("File structure for the new case has been created!")
+    #msg.setInformativeText("This is additional information")
+    msg.setWindowTitle("New Case")
+    msg.setDetailedText("Directory created in OpenFOAM home directory: $FOAM_RUN")
+    msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+    retval = msg.exec_()
+    print("value of pressed message box button:", retval)
+    
 def msgbtn(i):
-   	print "Button pressed is:",i.text()
+    print("Button pressed is:",i.text())
